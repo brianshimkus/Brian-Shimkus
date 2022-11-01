8cd33fee3aa9dec413f992ad25b4e15773eadf2f
Brian Shimkus <brianshimkus1@gmail.com>
2022-10-31 23:47:34 -0500

    Set up menu

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,9 +1,19 @@
+import Link from 'next/link'
+
 export default function Header() {
 	return (
-		<div className='bg-gray-700'>
-			<div className='container mx-auto'>
-				<h1 className='text-3xl'>Menu</h1>
+		<>
+			<div className='container mx-auto flex text-2xl py-2'>
+				<Link href='/'>
+					<h1 className='text-rose-300'>Brian Shimkus</h1>
+				</Link>
+				<span className='flex-grow' />
+				<nav className='ml-12 space-x-8'>
+					<Link href='/about'>About</Link>
+					<Link href='/projects'>Projects</Link>
+					<Link href='/contact'>Contact</Link>
+				</nav>
 			</div>
-		</div>
+		</>
 	)
 }
